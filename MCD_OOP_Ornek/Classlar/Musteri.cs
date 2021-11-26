2f8a0783fd1084b21d37dc0f42e54f7eeecbcd63
Faik Turan <turanfaik@hotmail.com>
2021-11-26 21:08:01 +0300

    Add project files.

---
+++ MCD_OOP_Ornek/Classlar/Musteri.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOP_Ornek.Classlar
+{
+    class Musteri
+    {
+        #region Properties
+
+        public int MusteriId { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public Cinsiyetler Cinsiyeti { get; set; }
+        public UrunSepeti MusterininUrunSepeti { get; set; }
+
+        #endregion
+
+        #region Metotlar
+
+        public bool MusterininDogumGunuMu()
+        {
+            bool kontrol = false;
+
+            kontrol = (DogumTarihi.Day == DateTime.Now.Day && DogumTarihi.Month == DateTime.Now.Month) ? true : false;
+
+            return kontrol;
+        }
+
+        public void MusteriBilgileriYazdir()
+        {
+            Console.WriteLine($"Müşteri No:{MusteriId} - {MusteriAdi} {MusteriSoyadi}");
+        }
+
+        public void MusterininSepetiniYazdir()
+        {
+
+        }
+
+
+        #endregion
+    }
+}
