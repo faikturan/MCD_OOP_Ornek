99982a03151f1a047f23fff1f5afdfcf75ce6cdd
Faik Turan <turanfaik@hotmail.com>
2021-11-29 16:04:09 +0300

    code changed

--- MCD_OOP_Ornek/Classlar/Musteri.cs
+++ MCD_OOP_Ornek/Classlar/Musteri.cs
@@ -38,6 +38,22 @@ namespace MCD_OOP_Ornek.Classlar
         public void MusterininSepetiniYazdir()
         {
             
+            if (MusterininUrunSepeti.UrunlerListesi.Count > 0)
+            {
+                //Listeyi yazdırmadan önce doğum günü durumuna bakmanız gerekiyor.
+                if (MusterininDogumGunuMu() == true)
+                {
+                    MusterininUrunSepeti.DogumGunuHediyesiEkle();
+                    Console.WriteLine("Doğum gününüz kutlu olsun. Gofret hediyemiz sepete eklendi....");
+                }
+
+                int sayac = 1;
+                foreach (var item in MusterininUrunSepeti.UrunlerListesi)
+                {
+                    Console.WriteLine(sayac + " . ürünümüz: " +item.UrunAdi);
+                    sayac++;
+                }
+            }
         }
 
 
